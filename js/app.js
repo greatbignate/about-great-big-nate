17e6d250bef91f07824f30ca89c03bffdca44896
GREATBIGNATE <greatbignate@gmail.com>
2018-08-08 16:44:05 -0700

    minimum JS applications complete and in js/app

--- js/app.js
+++ js/app.js
@@ -66,10 +66,10 @@ if (answerFive === 'y' || answerFive === 'yes') {
 }
 
 // Sixth Question
-for (var tries = 0; tries <5; tries++) {
-    var pushUpGuess = random(1,10);
-    console.log(pushUpGuess);
-    answerSix = prompt('How many single-handed pushups can Nate do with his left arm on a given day?');
+for (var tries = 0; tries <3; tries++) {
+    var pushUpGuess = Math.ceil(5*Math.random());
+    //console.log(pushUpGuess);
+    answerSix = prompt('How many single-handed pushups can Nate do with his left arm on a given day? Hint: Less than 6');
     if (pushUpGuess == answerSix){
         alert ('Wow! Great guess! Not even Nate knows the answer to that question!');
         console.log('User was prompted to guess a random number which was ' +pushUpGuess+ ' in this case. User guessed the number after ' +tries+ ' tries.')
@@ -78,7 +78,7 @@ for (var tries = 0; tries <5; tries++) {
         var triesLeft = 5-tries;
         alert ('Good guess! Unfortunately, no one knows the answer to that question. Not even Nate! Try again!');
         alert ('You have ' +triesLeft+ ' tries left.')
-        console.log('User failed to guess a random number which was ' +pushUpGuess+ ' in this case. User has' +triesLeft+ 'attempt remaining.');
+        console.log('User failed to guess a random number which was ' +pushUpGuess+ ' in this case. User has ' +triesLeft+ ' attempt remaining.');
     }
 }
 
--- js/experiment.js
+++ js/experiment.js
@@ -1,16 +1,28 @@
-// Sixth Question
-for (var tries = 0; tries <5; tries++) {
-    var pushUpGuess = random(1,10);
-    console.log(pushUpGuess);
-    answerSix = prompt('How many single-handed pushups can Nate do with his left arm on a given day?');
-    if (pushUpGuess == answerSix){
-        alert ('Wow! Great guess! Not even Nate knows the answer to that question!');
-        console.log('User was prompted to guess a random number which was ' +pushUpGuess+ ' in this case. User guessed the number after ' +tries+ ' tries.')
-        break
-    } else {
-        var triesLeft = 5-tries;
-        alert ('Good guess! Unfortunately, no one knows the answer to that question. Not even Nate! Try again!');
-        alert ('You have ' +triesLeft+ ' tries left.')
-        console.log('User failed to guess a random number which was ' +pushUpGuess+ ' in this case. User has' +triesLeft+ 'attempt remaining.');
+// Seventh Question
+
+// Initial variables set
+var answerSeven = [];
+var faveMovies = ['shane','casablanca','the godfather','godfather','thegodfather','star wars','starwars','pulp fiction','pulpfiction'];
+var triesLeft = 5;
+var movGuess = prompt('Guess my top five movies! What is your first guess? You have -6- tries remaining!').toLowerCase();
+var hits = 0;
+
+// They get 6 guesses
+while (triesLeft>=0) {
+
+    answerSeven.push(movGuess); //Answer Seven will ultimately be an array of thier 6 guesses.
+    i=0;
+    
+    // This while loop should compare the current movGuess try to each of the faveMovie entries.
+    for (var i=0 ; i < 9 ; i++) {
+        if (movGuess === faveMovies[i]){
+            alert('Wow! Great guess!'); // Acknowledges a correct guess    
+            hits++; // Tallies number of correct guesses
+        }
     }
+    // Now that the current movGuess has been compared to the possible answers, we need to move on to the next guess.    
+    var movGuess = prompt('What is your next guess? You have ' +hits+ ' correct guesses and ' +triesLeft+ ' attempts remaining!').toLowerCase();
+    triesLeft--;
 }
+
+alert ('Recap: My favorite movies are: Shane, Casablanca, The Godfather, Star Wars, and Pulp Fiction! You guessed ' +answerSeven+ '. Good guessing!');
